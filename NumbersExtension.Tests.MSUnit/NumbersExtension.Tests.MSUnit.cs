6f5db6bbd447c00a99b121cd8105299d25632878
PashaFast <pashahit@gmail.com>
2020-01-21 21:09:14 +0300

    Task is done

---
+++ NumbersExtension.Tests.MSUnit/NumbersExtension.Tests.MSUnit.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NumbersExtension.Tests.MSUnit
+{
+    [TestClass]
+    public class NumbersExtensionTests
+    {
+        [TestMethod]
+        public void InsertNumberTest()
+        {
+            int expected = 345346;
+
+            int result = NumbersExtension.InsertNumberIntoAnother(-55465467, 345346, 0, 31);
+
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod]
+        public void InsertNumberIntoAnotherWith14_50_Return_22()
+        {
+            int sourceNumber = 14;
+            int numberln = 50;
+            int i = 3;
+            int j = 6;
+            int expected = 22;
+            int actual = NumbersExtension.InsertNumberIntoAnother(sourceNumber, numberln, i, j);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertNumberIntoAnotherWithBigNumbers()
+        {
+            int sourceNumber = 554216104;
+            int numberln = 4460559;
+            int i = 11;
+            int j = 18;
+            int expected = 554203816;
+            int actual = NumbersExtension.InsertNumberIntoAnother(sourceNumber, numberln, i, j);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SInsertNumberIntoAnother_Throw_ArgumentException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => NumbersExtension.InsertNumberIntoAnother(10, 45, 23, 12),
+                message: "Method generates ArgumentException in case i more then j");
+        }
+
+    }
+}
---
+++ NumbersExtension.Tests.NUnit/NumbersExtension.Tests.NUnit.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System;
+
+namespace NumbersExtension.Tests.NUnit
+{
+    public class NumberExtensionTests
+    {
+        [TestCase(2728, 655, 3, 8, ExpectedResult = 2680)]
+        [TestCase(554216104, 15, 0, 31, ExpectedResult = 15)]
+        [TestCase(-55465467, 345346, 0, 31, ExpectedResult = 345346)]
+        [TestCase(554216104, 4460559, 11, 18, ExpectedResult = 554203816)]
+        [TestCase(-1, 0, 31, 31, ExpectedResult = 2147483647)]
+        [TestCase(-2147483648, 2147483647, 0, 30, ExpectedResult = -1)]
+        [TestCase(-2223, 5440, 18, 23, ExpectedResult = -16517295)]
+        [TestCase(2147481425, 5440, 18, 23, ExpectedResult = 2130966353)]
+        public int InsertNumbers_SomeNumbers_ExpectedResult(int numberSource, int numberIn, int i, int j)
+        {
+            return NumbersExtension.InsertNumberIntoAnother(numberSource, numberIn, i, j);
+        }
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentException() =>
+            Assert.Throws<ArgumentException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 8, 3),
+                message: "i must be less then j");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_Less() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, -1, 3),
+                message: "index must be positive");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_More() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 32, 32),
+                message: "index must be less then 32");
+
+        [Test]
+        public void InsertNumberIntoAnother_ArgumentOutOfRangeException_MoreAndLess() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => NumbersExtension.InsertNumberIntoAnother(8, 15, 0, 32),
+                message: "index must be less then 32");
+
+    }
+}
